8c170c500da63f1121875fba59ca7e9dc5d12a3e
Marcin B <mbarzyk@atlassian.com>
2024-08-18 18:36:45 +0200

    PX-2: Heyyyyy

--- Heyo.java
+++ Heyo.java
@@ -2,5 +2,7 @@ public class Heyo {
     public static void main(String[] args) {
         System.out.println("yo");
         System.out.println("not really");
+
+        System.out.println("KEK");
     }
 }
